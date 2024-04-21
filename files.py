ff288d0edd84a667cd04ddb5e641e987c0dfefcb
Palak Srivastava <srivastavapalak1604@gmail.com>
2024-04-21 12:17:54 +0530

    this is our project initials

--- files.py
+++ files.py
@@ -77,3 +77,12 @@ while i<=3:
             k += 1
         j += 1
     i += 1
+
+# WAP to print square root and cube roots  from 1 to 10
+import math
+width =10
+precision = 4
+for n in range(1,10):
+    s=math.sqrt(n)
+    c=math.pow(n,1/3)
+    print(f'{n^5}{s:{width}.{precision}}{c:{width}.{precision}}')
